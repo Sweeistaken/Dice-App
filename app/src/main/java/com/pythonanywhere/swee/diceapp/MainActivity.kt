76b94e285d6e02bdb7870ba134bc2180b2e7e10b
Swee <sweeistaken@gmail.com>
2024-01-21 18:42:56 -0800

    some bugfixes and cleaning

--- app/src/main/java/com/pythonanywhere/swee/diceapp/MainActivity.kt
+++ app/src/main/java/com/pythonanywhere/swee/diceapp/MainActivity.kt
@@ -1,92 +1,94 @@
 package com.pythonanywhere.swee.diceapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    fun toast(message: CharSequence) =
+    private fun toast(message: CharSequence) =
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var random = 0
-        val sides: EditText = findViewById(R.id.editTextNumber2) //Define the Sides TextBox
-        val dices: EditText = findViewById(R.id.editTextNumber) //Define the Sides TextBox
-        val rollButton: Button = findViewById(R.id.button) //Defines the Roll Button
-        val image: ImageView = findViewById(R.id.imageView) //Defines the First Image
-        val image2: ImageView = findViewById(R.id.imageView2) //Defines the Second image
-        val image3: ImageView = findViewById(R.id.imageView3) //Defines the Third image
-        val images: CheckBox = findViewById(R.id.checkBox) //Defines the "Images" checkbox
+        val nope = R.drawable.nope
+        val sides: EditText = findViewById(R.id.editTextNumber2)
+        val dices: EditText = findViewById(R.id.editTextNumber)
+        val rollButton: Button = findViewById(R.id.button)
+        val image: ImageView = findViewById(R.id.imageView)
+        val image2: ImageView = findViewById(R.id.imageView2)
+        val image3: ImageView = findViewById(R.id.imageView3)
+        val images: Switch = findViewById(R.id.switch1)
         val ones = listOf(R.drawable.one,R.drawable.two,R.drawable.three,R.drawable.four,R.drawable.five,R.drawable.six,R.drawable.seven,R.drawable.eight,R.drawable.nine,R.drawable.ten) //maps all the Dice images
-        rollButton.setOnClickListener {// When the Roll button is clicked
+        rollButton.setOnClickListener {
             try {
-                image.visibility = View.INVISIBLE // Hides the images first:
-                image2.visibility = View.INVISIBLE // :
-                image3.visibility = View.INVISIBLE // :End
-                val abc = try {sides.text.toString().toInt()}catch (e:Exception){0} //Turns the value of the sides TextBox to int
-                val abcd = try {dices.text.toString().toInt()}catch (e:Exception){0} //Turns the value of the dice TextBox to int
-                val resultTextView: TextView = findViewById(R.id.vales) //Defines the Result Text
-                var vals = listOf((1..abc).random()) //List of values
-                if (abcd > 30) {
+                image.visibility = View.INVISIBLE
+                image2.visibility = View.INVISIBLE
+                image3.visibility = View.INVISIBLE
+                val temp1 = try {sides.text.toString().toInt()}catch (e:Exception){0}
+                val temp2 = try {dices.text.toString().toInt()}catch (e:Exception){0}
+                val resultTextView: TextView = findViewById(R.id.vales)
+                var values = listOf((1..temp1).random())
+                if (temp2 > 30) {
                     toast("The dice value is abnormally big! You will get visual issues!")
                 }
-                if (abc > 500) {
+                if (temp1 > 500) {
                     toast("The sides value is abnormally big! You will get visual issues and/or lag!")
                 }
-                for (dice in 2..dices.text.toString().toInt()) { //for loop to make random numbers
-                    random = (1..abc).random()
-                    vals = vals.plus(random)
+                for (dice in 2..dices.text.toString().toInt()) {
+                    val num = (1..temp1).random()
+                    values = values.plus(num)
                 }
-                var lists = "" //lists is the string to compile the list to a string
-                if (vals.count() == 1) { //Validates the requirements to use images
+                var lists = ""
+                if (values.count() == 1) {
                     if (images.isChecked and (dices.text.toString() == "1") and (sides.text.toString().toInt() <= 10)) { //makes image 1 visible
                         image.visibility = View.VISIBLE
-                        image.setImageResource(ones.get(vals.get(0) - 1))
+                        image.setImageResource(ones[values[0] - 1])
                     }else {
                         if (images.isChecked) {
                             image.visibility = View.VISIBLE
-                            image.setImageResource(R.drawable.nope)
+                            image.setImageResource(nope)
                         }
                     }
 
 
-                    lists = vals.get(0).toString()
+                    lists = values[0].toString()
                 }else {
-                    if (images.isChecked and (dices.text.toString() == "2") and (sides.text.toString().toInt() <= 10)) { //makes image 1 and 2 visible and sets it to their respective images
+                    if (images.isChecked and (dices.text.toString() == "2") and (sides.text.toString().toInt() <= 10)) {
                         if (sides.text.toString().toInt() < 11) {
                             image.visibility = View.VISIBLE
-                            image.setImageResource(ones.get(vals.get(1) - 1))
+                            image.setImageResource(ones[values[1] - 1])
                             image2.visibility = View.VISIBLE
-                            image2.setImageResource(ones.get(vals.get(0) - 1))
+                            image2.setImageResource(ones[values[0] - 1])
                         } else {
 
                             image.visibility = View.VISIBLE
-                            image.setImageResource(R.drawable.nope)
+                            image.setImageResource(nope)
                         }
 
-                    }else if (images.isChecked and (dices.text.toString() == "3") and (sides.text.toString().toInt() < 11)) { //makes all images visible and sets it to their respective images
+                    }else if (images.isChecked and (dices.text.toString() == "3") and (sides.text.toString().toInt() < 11)) {
                         if (sides.text.toString().toInt() < 11) {
                             image.visibility = View.VISIBLE
-                            image.setImageResource(ones.get(vals.get(1) - 1))
+                            image.setImageResource(ones[values[1] - 1])
                             image2.visibility = View.VISIBLE
-                            image2.setImageResource(ones.get(vals.get(0) - 1))
+                            image2.setImageResource(ones[values[0] - 1])
                             image3.visibility = View.VISIBLE
-                            image3.setImageResource(ones.get(vals.get(2) - 1))
+                            image3.setImageResource(ones[values[2] - 1])
                         } else {
                             image.visibility = View.VISIBLE
-                            image.setImageResource(R.drawable.nope)
+                            image.setImageResource(nope)
                         }
                     }else if (images.isChecked) {
                         image.visibility = View.VISIBLE
-                        image.setImageResource(R.drawable.nope)
+                        image.setImageResource(nope)
                     }
-                    for (i in vals) { // compiles the list to lists
-                        lists += i.toString() + "  "
+                    for (i in values) {
+                        lists += "$i  "
                     }
                 }
-                resultTextView.text = lists // sets the textView to the compiled list
+                resultTextView.text = lists
             }catch(e: NoSuchElementException){
                 val resultTextView: TextView = findViewById(R.id.vales)
                 resultTextView.text = getString(R.string.sideserror)
